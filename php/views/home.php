8d0dcb6a09418b6e90db4f1f6021327f6e903a9c
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-04-21 15:02:20 +0200

    Splits

--- php/views/home.php
+++ php/views/home.php
@@ -6,19 +6,19 @@ require_once "{$_SERVER['DOCUMENT_ROOT']}/php/models/Resources.php";
       <div class="page">
         <div class="pageContent">
           <img class="mainImage" src="/img/main.JPG" alt="Koch">
-          <h2 class="title main">Gastgeber aus Leidenschaft</h2>
-          <h3 class="subtitle">freude & genuss wo immer sie sind</h3>
-          <p>
-            Die Gastronomie wird stets dynamischer -<br/>
-            Sirana Catering bringt Ihre Wünsche zu Ihnen.
-            Lassen Sie uns Ihre Vorstellungen wissen und wir zaubern Ihren Wunschanlass.
-          </p>
+          <h2 class="title main top-space">Gastgeber aus Leidenschaft</h2>
+          <h3 class="subtitle bot-space">freude & genuss wo immer sie sind</h3>
 
-          <h2 class="title">Wieso Sirana</h2>
-          <p>Sirana ist ein innovatives Catering Unternehmen mit Sitz in Glarus. Unsere Stärke ist nicht nur die
-            kulinarische Vielfalt, sondern insbesonderea auch die Individualität unsere Kundenschaft. Um Ihre
-            Zufriedenheit sicherzustellen, betrachten wir jeden Anlass als neues Projekt und passen dieses Ihren
-            individuellen Wünschen an.</p>
+          <div class="split">
+            <div class="right">
+              <h2 class="title">Wieso Sirana</h2>
+              <p>Sirana ist ein innovatives Catering Unternehmen mit Sitz in Glarus. Unsere Stärke ist nicht nur die
+                kulinarische Vielfalt, sondern insbesondere auch die Individualität unsere Kundenschaft. Um Ihre
+                Zufriedenheit sicherzustellen, betrachten wir jeden Anlass als neues Projekt und passen dieses Ihren
+                individuellen Wünschen an.</p>
+            </div>
+            <img src="/img/barkeeper.jpg" alt="Barkeeper" class="left">
+          </div>
           <p class="quote">"Wir machen alles, um Ihnen eine Freude zu bereiten und teilen mit Ihnen die Leidenschaft
             fürs Essen."</p>
           <img class="fruit" src="<?php echo Resources::getRandomStaticFruit() ?>" alt="Fruit">
