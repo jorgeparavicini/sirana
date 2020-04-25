deb7932c2bf519d1db46cbe559e817bc443cb01f
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-04-25 18:32:13 +0200

    Main refactored

--- php/views/home.php
+++ php/views/home.php
@@ -1,43 +1,41 @@
 <?php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/php/models/Resources.php";
 ?>
-  <div class="pageContent">
+  <div class="pageContent" id="home">
     <img class="mainImage" src="/img/main.JPG" alt="Koch">
-    <h2 class="title main top-space">Gastgeber aus Leidenschaft</h2>
-    <h3 class="subtitle">freude & genuss wo immer sie sind</h3>
+    <h1 class="title main top-space">Gastgeber aus Leidenschaft</h1>
+    <h2>freude & genuss wo immer sie sind</h2>
 
     <div class="split">
       <div class="left">
-        <h2 class="title">Wieso Sirana</h2>
+        <h3>Wieso Sirana</h3>
         <p>Sirana ist ein innovatives Catering Unternehmen mit Sitz in Glarus. Unsere Stärke ist nicht nur die
           kulinarische Vielfalt, sondern insbesondere auch die Individualität unsere Kundenschaft. Um Ihre
           Zufriedenheit sicherzustellen, betrachten wir jeden Anlass als neues Projekt und passen dieses Ihren
           individuellen Wünschen an.</p>
       </div>
-      <div class="right">
-        <img src="/img/barkeeper.jpg" alt="Barkeeper">
-      </div>
+      <img class="right" src="/img/barkeeper.jpg" alt="Barkeeper">
     </div>
     <p class="quote">"Wir teilen mit Ihnen die Leidenschaft für Ihren Anlass"</p>
     <!--          <img class="fruit" src="-->
     <?php //echo Resources::getRandomStaticFruit() ?><!--" alt="Fruit">-->
-    <div class="red">
-      <h2 class="title">Unsere Philosophie</h2>
+    <div class="banner">
+      <h3>Unsere Philosophie</h3>
       <p>Familie, Freunde und die Leidenschaft für gutes Essen sind ein grosser Teil unserer Philosophy und bilden
         den Ursprung unserer Unternehmung. Kleine Familienfesten und Anlässe mit Freunden bringen den Ball ins
         Rollen... Aus Hobby wird Leidenschaft, asu Leidenschaft wird Beruf...</p>
     </div>
 
     <div class="split">
-      <div class="right">
-        <h2 class="title kochstil">Unser Kochstil</h2>
+      <div class="left">
+        <h3>Unser Kochstil</h3>
         <p>Mit frischen Zutaten von wo möglich lokalen Partnern kreiren wir saisonale Gerichte mit internationalem
           Touch, die Sie auf eine kulinarische Weltreise mitnehmen. Wir servieren Speiseen, die Freude bereiten.
           Sie
           sollen geschmacklich überzeugen und Ihren Wünschen gerecht werden.</p>
 
       </div>
-      <img src="/img/frei.png" alt="Frei" class="left pink">
+      <img src="/img/frei.png" alt="Frei" class="right pink">
     </div>
 
     <p class="quote">Lassen Sie uns also wissen, mit welchen Gerichten wir Ihnen eine besondere Freude
