a30ab94ec9968ae01d768995d7f7c6db01ba016f
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-05-04 10:59:58 +0200

    Footer new stuff

--- php/static/footer.php
+++ php/static/footer.php
@@ -1,4 +1,26 @@
 <footer>
-  <a href="">Impressum</a>
-  <a href="">Datenschutz</a>
+  <div class="column">
+    <address>
+      Sirana GmbH<br>
+      Dorfstrasse 35<br>
+      8755 Ennenda<br>
+    </address>
+  </div>
+  <div class="column" id="center">
+    <div>
+      <a href=""><img src="https://img.icons8.com/ios-glyphs/30/000000/facebook-new.png" alt="Facebook"/></a>
+      <a href=""><img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png" alt="Instagram"/></a>
+      <a href=""><img src="https://img.icons8.com/ios-glyphs/30/000000/phone--v1.png" alt="Telephone"/></a>
+    </div>
+    <div>
+      <a href="" class="legal right">Impressum</a>
+      <a href="" class="legal left">Datenschutz</a>
+    </div>
+    <div class="icons8">
+      <p>Icons provided by
+        <a href="https://icons8.com/">Icons8</a>
+      </p>
+    </div>
+  </div>
+  <div></div>
 </footer>
