6bd6afcc540e8f20e69942e0c553ff86c76e4c63
Jorge Paravicini <j.m.paravicini@gmail.com>
2020-05-17 22:42:28 +0200

    Cookie consent

--- php/static/head.php
+++ php/static/head.php
@@ -9,7 +9,11 @@ function build_head(string $file)
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166849267-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+
       gtag('js', new Date());
 
       gtag('config', 'UA-166849267-1');
@@ -18,8 +22,9 @@ function build_head(string $file)
     <title><?php echo ucfirst($filename) ?> | Sirana </title>
 
     <meta charset="UTF-8">
-    <?php // TODO: Implement ?>
-    <meta name="*DESCRIPTION*" content="*CONTENT*">
+    <?php // TODO: Implement
+    ?>
+    <meta name="description" content="*CONTENT*">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#fafafa">
 
@@ -29,6 +34,29 @@ function build_head(string $file)
 
     <script src="/js/jquery.js"></script>
     <script src="/js/header.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="//wpcc.io/lib/1.0.2/cookieconsent.min.css"/>
+    <script src="//wpcc.io/lib/1.0.2/cookieconsent.min.js"></script>
+    <script>
+      window.addEventListener("load", function () {
+        window.setTimeout(function () {
+          window.wpcc.init({
+            "colors": {
+              "popup": {"background": "#ffffff", "text": "#000000", "border": "#f3b09a"},
+              "button": {"background": "#f3b09a", "text": "#ffffff"}
+            },
+            "border": "thin",
+            "corners": "small",
+            "content": {
+              "href": "https://sirana.ch/datenschutz",
+              "message": "Diese Website verwendet Cookies, um Ihnen die bestmögliche Nutzung unserer Website zu gewährleisten.",
+              "link": "Mehr erfahren",
+              "button": "Ich akzeptiere"
+            }
+          })
+        }, 5000);
+      })
+    </script>
   </head>
 
   <?php
